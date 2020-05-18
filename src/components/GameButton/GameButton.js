4e3c9dcc519363177d5a5ff35d9e3151906b44a1
Isael Lizama <isaellizama@gmail.com>
2020-05-18 13:42:09 -0600

    added timeouts but need to clear them properly

--- src/components/GameButton/GameButton.js
+++ src/components/GameButton/GameButton.js
@@ -3,25 +3,46 @@ import React, {useState, useEffect} from 'react'
 const GameButton = ({func,id, clickedLib, reset, setReset}) => {
     const[clicked, setClicked] = useState(false)
     const [hidden, setHidden] = useState(false)
-
+    const [limit, setLimit] = useState(false)
     console.log(clickedLib.length)
 
     useEffect(() => {
-        if(reset && !hidden) setClicked(false)
-        if(clickedLib.length === 2 && clickedLib[0] === clickedLib[1]) {
-           if(clickedLib.length === 2 && clickedLib[0] === id) setHidden(true)
+        if(reset && !hidden) {
+            setClicked(false)
+            setReset(false)
+            setLimit(false)
         }
+        if(clickedLib.length === 2){
+            func([])
+            setLimit(true)
+        }
+        
+        if((clickedLib.length === 2 && clickedLib[0] === clickedLib[1]) && (clickedLib.length === 2 && clickedLib[0] === id)){
+            setTimeout(() => {
+                setHidden(true)
+                
+            }, 1000);
+        }
+        
     })
+    useEffect(() => {
+        if(limit){
+            setTimeout(() => {
+                setReset(true)
+            }, 1000);
+        }
+    },[limit])
     
     const onButtonClick = () => {
         setClicked(true)
         if(clickedLib.length < 2){
             func([...clickedLib, id])
-            setReset(false)
-        } else {
-            func([])
-            setReset(true)
-        }
+            // setTimeout(() => {
+                setReset(false)
+            // }, 1000);
+        } 
+
+        
     }
 
     return (
